d2f685af33595ff3fc7137480a814398ea18d57f
Bernard Mulaw <bernard@nat-wireless-guest-reg-153-50.bu.edu>
2022-03-13 17:31:19 -0400

    backend sets up mongo database

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -3,8 +3,12 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import json
-
+from flask import Flask
+from flask_pymongo import PyMongo
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/musicMap"
+mongodb_client = PyMongo(app)
+db = mongodb_client.db
 cities = ["New_York_City", "Los_Angeles", "Brooklyn", "Seattle", "Houston", "Detroit", "Philadelphia", "Nashville"]
 artist_map = {}
 for city in cities:
@@ -22,5 +26,10 @@ for city in cities:
                 end = singer.index('">')
                 new_data.append(singer[start:end])
     artist_map[city] = new_data
+    if city == "Brooklyn": print(city, new_data)
+
+    """
+['Aaliyah', 'Red Cafe', 'Jasmine Cephas Jones', 'Cheryl Clemons', 'Maino (rapper)', 'Melora Creager', 'Dana Dane', "Da'Vinchi", 'Mos Def', 'Neil Diamond', 'David Draiman', 'Erick Arc Elliott', 'The Epochs', 'Fabolous', 'Lyric (group)', 'Cristina Fontanelli', 'Fu-Schnickens', 'Deborah Gibson', 'Johnny Gioeli', 'Ben Goldwasser', 'Louise Gunning', 'Arlo Guthrie', 'GZA', 'Adelaide Hall', 'Richie Havens', 'Lena Horne', 'Cheryl James', 'Jay-Z', 'Jaz-O', 'Jeru the Damaja', 'Joey Badass', 'Just-Ice', 'KA (rapper)', 'Big Daddy Kane', 'Lainie Kazan', 'The Kid Gashi', 'Carole King', 'Talib Kweli', 'Abbe Lane', 'Cyndi Lauper', 'Steve Lawrence', 'Shulem Lemmer', "Lil' Kim", 'Lil Mama', 'MC Lyte', 'Barry Manilow', 'Constantine Maroulis', 'Angie Martinez', 'Masta Ace', 'Maxwell (musician)', 'Meechy Darko', 'Robert Merrill', 'Matthew Paul Miller', 'Stephanie Mills', "Mr. Muthafuckin' eXquire", 'Uncle Murda', 'Harry Nilsson', 'The Notorious B.I.G.', 'O.C. (rapper)', "Ol' Dirty Bastard", 'Joell Ortiz', 'Eddie Rabbitt', 'Marky Ramone', 'Anthony Ramos (actor)', 'Lou Reed', 'Bebe Rexha', 'Buddy Rich', 'Rowdy Rebel', 'Brenda Russell', 'RZA', 'Saigon (rapper)', 'Evie Sands', 'Seymour Schwartzman', 'Raymond Scott', 'Neil Sedaka', 'Shabazz the Disciple', 'Bobby Shmurda', 'Beverly Sills', 'Justine Skye', 'Pop Smoke', 'DJ Spinderella', 'Peter Steele', 'Connie Stevens', 'David Stones', 'Barbra Streisand', 'Eric Stuart', 'Rachel Trachtenburg', 'Kathy Troccoli', 'Uncle Murda', 'Adam Yauch']
+    """
             
 print(artist_map.keys())
